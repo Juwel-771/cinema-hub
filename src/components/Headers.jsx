10b07fe187402383f8cb5bb21e023ef16cfdb228
Juwel-771 <juwelah99@gmail.com>
2025-05-04 00:47:57 +0600

    project file and folder

---
+++ src/components/Headers.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const header = () => {
+    return (
+        <header>
+            <h2>Header</h2>
+        </header>
+    );
+};
+
+export default header;
---
+++ src/components/index.jsx
@@ -0,0 +1,5 @@
+// export { Header } from './Header';
+export { Headers } from './Headers'
+export { Footer } from './Footer'
+export { Card } from './Card'
+
