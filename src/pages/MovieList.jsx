39dd1326bb019208412ba23b1f0e4786eda08140
Juwel-771 <juwelah99@gmail.com>
2025-05-05 21:41:06 +0600

    header and footer section created

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Headers from '../components/Headers';
+import Footer from '../components/Footer';
 
 const MovieList = () => {
     return (
         <main>
             <Headers></Headers>
+            <h1>MOVIE LIST</h1>
+            <Footer></Footer>
         </main>
     );
 };
