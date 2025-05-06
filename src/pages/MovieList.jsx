871e6ca8cf60aa4ab3249d0de913c4c9f1efe9b1
Juwel-771 <juwelah99@gmail.com>
2025-05-06 06:06:16 +0600

    movie card created

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import Headers from '../components/Headers';
 import Footer from '../components/Footer';
+import { Link } from 'react-router-dom';
+import Card from '../components/Card';
 
 const MovieList = () => {
     return (
-        <main>
-            <Headers></Headers>
-            <h1>MOVIE LIST</h1>
-            <Footer></Footer>
+       <>
+       <Headers></Headers>
+        <main className='dark:bg-slate-800'>
+            <section className='max-w-7xl mx-auto py-7'>
+                <div className='flex justify-start flex-wrap'>
+                    <Card></Card>
+                    <Card></Card>
+                    <Card></Card>
+                    <Card></Card>
+                    <Card></Card>
+                    <Card></Card>
+                </div>
+            </section>
         </main>
+       <Footer></Footer>
+       </>
     );
 };
 
