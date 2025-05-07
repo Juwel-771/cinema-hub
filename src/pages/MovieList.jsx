cc8e68bc34ffea827f314295a10a7e4cf9f2765f
Juwel-771 <juwelah99@gmail.com>
2025-05-07 13:27:25 +0600

    useHooks created

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -3,20 +3,24 @@ import Headers from '../components/Headers';
 import Footer from '../components/Footer';
 import { Link } from 'react-router-dom';
 import Card from '../components/Card';
+import useFetch from '../hooks/useFetch';
+
 
 const MovieList = () => {
 
-    const [movies, setMovies] = useState([]);
+    // const [movies, setMovies] = useState([]);
+    const {data : movies} = useFetch("https://api.themoviedb.org/3/movie/now_playing?api_key=abef22202ef4bbd29941b8c79553fc53");
 
-    useEffect(() => {
-        async function fetchMovies(){
-            const response = await fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=abef22202ef4bbd29941b8c79553fc53");
-            const data = await response.json();
-            setMovies(data.results);
-        }
 
-        fetchMovies(); 
-    }, [])
+    // useEffect(() => {
+    //     async function fetchMovies(){
+    //         const response = await fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=abef22202ef4bbd29941b8c79553fc53");
+    //         const data = await response.json();
+    //         setMovies(data.results);
+    //     }
+
+    //     fetchMovies(); 
+    // }, [])
 
     return (
        <>
